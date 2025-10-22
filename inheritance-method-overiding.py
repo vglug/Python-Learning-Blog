fdb3be4a4559de4a8c5ac318e121e862f35cb744
Praveenkanth G <praveenkanth2805@gmail.com>
2025-10-22 18:53:35 +0530

    add comments for inheritance-method-overriding.py

--- inheritance-method-overiding.py
+++ inheritance-method-overiding.py
@@ -1,20 +1,20 @@
-class assignment():
-    def __init__(s,name,n):
+class assignment():    #create a base class for overriding
+    def __init__(s,name,n):    #it is assign the value
         s.name=name
         s.n=n
         s.submit()
-    def submit(s):
+    def submit(s):    #overriding function
          pass
-    def mark(s):
+    def mark(s):    #overriding function
         pass
-class dbmsAssignment(assignment):
-    def submit(s):
-        s.Ano=[]
+class dbmsAssignment(assignment):    #child class of assignment
+    def submit(s):    #overriding function
+        s.Ano=[]    #list for give marks
         for i in range(s.n):
-            s.Ano.append(i)
-        print(f"{s.name} is submited {type(s).__name__[:4]}  assignment {s.n}")
-        s.mark()
-    def mark(s):
+            s.Ano.append(i)    #append the assignmet no in list 
+        print(f"{s.name} is submited {type(s).__name__[:4]}  assignment {s.n}")    #print the student is submitted the assignment
+        s.mark()    # call the abstract method as overriding function
+    def mark(s):    #overriding function and it assign the mark depend submitted assignment
         if len(s.Ano)==1:
             print("mark:12")
         elif len(s.Ano)==2:
@@ -25,14 +25,14 @@ class dbmsAssignment(assignment):
             print("mark:22")
         elif len(s.Ano)==5:
             print("mark:24")
-class osAssignment(assignment):
-    def submit(s):
+class osAssignment(assignment):    #child class of assignment
+    def submit(s):     #overriding function
         s.Ano=[]
         for i in range(s.n):
-            s.Ano.append(i)
-        print(f"{s.name} is submited {type(s).__name__[:2]}  assignment {s.n}")
-        s.mark()
-    def mark(s):
+            s.Ano.append(i)    #append the assignmet no in list
+        print(f"{s.name} is submited {type(s).__name__[:2]}  assignment {s.n}")    #print the student is submitted the assignment
+        s.mark()    # call the abstract method as overriding function
+    def mark(s):    #overriding function and it assign the mark depend submitted assignment
         if len(s.Ano)==1:
             print("mark:12")
         elif len(s.Ano)==2:
@@ -43,9 +43,8 @@ class osAssignment(assignment):
             print("mark:22")
         elif len(s.Ano)==5:
             print("mark:24")
-        else:
-             ()
-            
+
+#object for assignments
 s1=dbmsAssignment("praveenkanth",1)
 s1=osAssignment("praveenkanth",1)
 s1=dbmsAssignment("praveenkanth",2)
