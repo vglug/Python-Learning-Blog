babc9acc53cc8ebdb469dce689a0cb1d24ddc878
deepana0206 <deepanaganapathy06@gmail.com>
2025-10-26 05:29:16 +0530

    if else program
    added comments

--- if-else-program.py
+++ if-else-program.py
@@ -1,11 +1,16 @@
 def shop():
-    price=int(input("Enter a price:"))
-    if price>=1000 and price<=2000:
-        print("you got a 200 rs gift voucher")
-    elif price>=2000 and price<=3000:
-        print("you got a 400 rs gift voucher")
-    elif price>=3000 and price<=4000:
-        print("you got a 600 rs gift voucher")
-    else:
+    price = int(input("Enter a price:")) # Ask the user to enter the price of the item
+
+    if price >= 1000 and price <= 2000: # If the price is between 1000 and 2000, give a 200 rs voucher
+        print("You got a 200 rs gift voucher")
+    
+    elif price >= 2000 and price <= 3000:  # If the price is between 2000 and 3000, give a 400 rs voucher
+        print("You got a 400 rs gift voucher")
+    
+    elif price >= 3000 and price <= 4000:   # If the price is between 3000 and 4000, give a 600 rs voucher
+        print("You got a 600 rs gift voucher")
+    
+    else:  # If the price is not in any of the above, no voucher
         print("No gift voucher")
-shop()        
+
+shop() #run the shop function
