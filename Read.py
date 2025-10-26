a0d8d8c0de225bc4a1074dfdd6a8683579cfe37c
kalaivani560 <kalaibharathi2604@gmail.com>
2025-10-26 15:55:28 +0530

    Add file reading and writing example in Read.py
    This program demonstrates how to read from and write to text files in Python.

--- Read.py
+++ Read.py
@@ -1,1 +1,16 @@
+# Program to read and write small text files
 
+# Writing to a text file
+with open("sample.txt", "w") as file:
+    file.write("Hello, this is a simple text file.\n")
+    file.write("We are learning how to read and write files in Python.\n")
+    file.write("File handling is easy and powerful!\n")
+
+print("Data has been written to sample.txt successfully.\n")
+
+# Reading from the same text file
+with open("sample.txt", "r") as file:
+    content = file.read()
+
+print("Contents of the file are:\n")
+print(content)
