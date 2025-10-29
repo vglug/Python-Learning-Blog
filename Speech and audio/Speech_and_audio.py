cf7b3d53266e6e6c9bf4ce1b08f315a4c9074703
Ambika K <ktyuvi003@gmail.com>
2025-10-29 23:11:10 +0530

    Update Speech_and_audio.py

--- Speech and audio/Speech_and_audio.py
+++ Speech and audio/Speech_and_audio.py
@@ -4,7 +4,7 @@ import numpy as np
 duration = 0.5  
 threshold = 0.3  
 
-print("Listening for Clap...")
+print("Listening for Clap....")
 
 while True:
     audio = sd.rec(int(duration * 44100), samplerate=44100, channels=1)
