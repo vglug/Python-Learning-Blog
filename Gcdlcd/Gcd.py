b5bb8392694d7bef76bac3301573fd5feb489d4e
Ambika K <ktyuvi003@gmail.com>
2025-10-30 17:59:08 +0530

    Update Gcd.py

--- Gcdlcd/Gcd.py
+++ Gcdlcd/Gcd.py
@@ -1,6 +1,6 @@
 import math
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
 
 gcd = math.gcd(a, b)
 lcm = abs(a * b) // gcd
